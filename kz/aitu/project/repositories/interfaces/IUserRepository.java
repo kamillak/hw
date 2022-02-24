92197517b78569b6cda87949fd2afab9703a979e
kamillak <99253623+kamillak@users.noreply.github.com>
2022-02-24 22:59:52 +0600

    UserRepo: added new methods

--- kz/aitu/project/repositories/interfaces/IUserRepository.java
+++ kz/aitu/project/repositories/interfaces/IUserRepository.java
@@ -1,4 +1,18 @@
 package kz.aitu.project.repositories.interfaces;
 
+import kz.aitu.project.entities.Movie;
+import kz.aitu.project.entities.Review;
+
+import java.util.List;
+
 public interface IUserRepository {
+    List<Movie> getAllMovies();
+    
+    List<Movie> orderMovies(String type);
+
+    List<String> browseMovie(String type);
+
+    boolean addReview(Review review);
+    double averageRating(int movieid);
+
 }
